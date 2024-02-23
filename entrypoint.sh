7d9abd0814a73d919b68e697704c960de6ac0d89
KitCat <161083973+kitcatto@users.noreply.github.com>
2024-02-23 16:30:39 -0500

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 2
+export HOME=/home/container
+cd /home/container
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+curl -Lo ./start.sh https://raw.githubusercontent.com/kitcatto/ptero-arm-ubuntu-egg/main/start.sh
+chmod +x ./start.sh
+# Run the Server
+bash ./start.sh
