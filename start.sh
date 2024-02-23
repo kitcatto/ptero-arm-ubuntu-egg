3b44f9b14c4d767f86e33256ff4f3587b06dcda8
KitCat <161083973+kitcatto@users.noreply.github.com>
2024-02-23 17:31:09 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Welcome to Ubuntu 22.04!"
 docmd() {
-    read -p "user@vps:~ " CMD
+    read -p "user@vps:$PWD~ " CMD
     eval "$CMD"
     docmd2
 }
 docmd2() {
-    read -p "user@vps:~ " CMD2
+    read -p "user@vps:$PWD~ " CMD2
     eval "$CMD2"
     docmd
 }
